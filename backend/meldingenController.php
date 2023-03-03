f7ffbff82f6d30b7f3819b2f2ec09320a673f004
daniongithub <danivandendobbelsteen@gmail.com>
2023-03-03 11:07:12 +0100

    y54t43q

--- backend/meldingenController.php
+++ backend/meldingenController.php
@@ -2,16 +2,28 @@
 
 //Variabelen vullen
 $attractie = $_POST['attractie'];
+$type = $_POST['type'];
 $capaciteit = $_POST['capaciteit']; 
 $melder = $_POST['melder'];
 
-echo $attractie . " / " . $capaciteit . " / " . $melder;
+echo $attractie . " / " . $type . " / " . $capaciteit . " / " . $melder;
+// var_dump($_POST);
+// die;
 
 //1. Verbinding
 require_once 'conn.php';
 
 //2. Query
+$query = "INSERT INTO meldingen (attractie, type, capaciteit, melder)
+VALUES(:attractie, :type, :capaciteit, :melder)";
 
 //3. Prepare
+$statement = $conn->prepare($query);
 
 //4. Execute
+$statement->execute([
+    ":attractie" => $attractie,
+    ":type" => $type,
+    ":capaciteit" => $capaciteit,
+    ":melder" => $melder,
+]);
